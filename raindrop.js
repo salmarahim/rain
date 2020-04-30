c807adbebac613adb4f88932321d007cb1b0e759
salmarahim <63525396+salmarahim@users.noreply.github.com>
2020-04-30 11:08:20 +0530

    Add files via upload

---
+++ raindrop.js
@@ -0,0 +1,18 @@
+class Raindrop{
+constructor(x,y,width,height){
+this.body=Bodies.rectangle(x,y,width,height);
+this.width=width;
+this.height=height;
+World.add(world,this.body);
+
+}
+display(){
+var pos=this.body.position;
+fill("white");
+rectMode(CENTER);
+rect(pos.x,pos.y,this.width,this.height);
+
+
+
+}
+}
---
+++ sketch.js
@@ -0,0 +1,19 @@
+var rimage;
+var r1;
+function preload(){
+
+
+  rimage=loadImage("sprites/k letter.jpg");
+}
+function setup() {
+
+ var canvas= createCanvas(800,400);
+
+}
+
+function draw() {
+  background(0);  
+  r1 =new Test();
+  drawSprites();
+  
+}
